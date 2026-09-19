b56ad5d65f8efb707391eb850e908136dabde057
Virexa-pixel <useingameasacount002@gmail.com>
2026-09-19 14:25:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/media-manager/',
   plugins: [react(), tailwindcss()],
 })
